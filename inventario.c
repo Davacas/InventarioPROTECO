8f85f7ee4153a90dd7c063caa2f5fac7f3c1bd6c
Davacas <danielbond09@hotmail.com>
2016-09-11 01:07:17 -0500

    Add files via upload
    PROYECTO FINALIZADO

--- inventario.c
+++ inventario.c
@@ -9,9 +9,12 @@ typedef struct Producto {
 } producto_t;
 
 int logueo ();
-producto_t* creaProductos (producto_t *);
+producto_t * validaArchivo (FILE *, producto_t *);
+producto_t * creaProductos (producto_t *);
 void llenaArchivo (FILE *, producto_t *);
 void imprimeInventario (FILE *, producto_t *);
+producto_t * actualizaCantidad (FILE *, producto_t *);
+producto_t * actualizaPrecio (FILE *, producto_t *);
 
 int main (int argc, char *argv[])
 {
@@ -21,37 +24,40 @@ int main (int argc, char *argv[])
 
 	if (logueo() == 1)
 	{
-		producto = creaProductos(producto);
-		llenaArchivo(archivo, producto);
+		producto = validaArchivo (archivo, producto);
+
 		do
 		{
 			system("clear");
-			printf("\n\n\t\tINVENTARIO ACTUAL DE 'EL PREBE INFELIZ':\n\n");
-			imprimeInventario(archivo, producto);
+			printf("\n\nINVENTARIO ACTUAL DE 'EL PREBE INFELIZ':\n\n");
+			printf("\tPRODUCTO    PRECIO   CANTIDAD\n\n");
+			imprimeInventario (archivo, producto);
 			printf("\n\n\t\t¿Qué deseas hacer?\n");
-			printf("\t\t\t1) Actualizar cantidad\n\t\t\t2) Actualizar precio\n\t\t\t3) Salir");
+			printf("\t\t\t1) Actualizar cantidad de un producto\n\t\t\t2) Actualizar precio de un producto\n\t\t\t3) Salir\t\t");
 			scanf("%d", &opc);
 
 			switch (opc)
 			{
-				case 1 :
-
-
+				case 1 : //ACTUALIZAR CANTIDAD
+					producto = actualizaCantidad (archivo, producto);
+					llenaArchivo (archivo, producto);
 					break;
-				case 2 :
 
-
+				case 2 : //ACTUALIZAR PRECIO
+					producto = actualizaPrecio (archivo, producto);
+					llenaArchivo (archivo, producto);
 					break;
-				case 3 :
+
+				case 3 : //SALIR DEL PROGRAMA
 					salir = 1;
 					break;
+
 				default :
-					printf(" ");	
+					opc = 0;	
 			}
 		} while (salir == 0);
 	}
 
-	fclose(archivo);
 	return 0;
 }
 
@@ -70,7 +76,7 @@ int logueo ()
 		printf("\n\t\t\tContraseña: ");
 		scanf(" %[^\n]", pass);
 
-		if (strcmp(usuario, "Davacas") == 0 && strcmp(pass, "davacas") == 0)
+		if (strcmp(usuario, "Donaldo") == 0 && strcmp(pass, "1994") == 0)
 			return acceso;
 		else if (strcmp(usuario, "Davacas") == 0 && strcmp(pass, "proteco123") == 0)
 			return acceso;
@@ -94,6 +100,28 @@ int logueo ()
 	return 0;
 }
 
+producto_t * validaArchivo (FILE *archivo, producto_t* producto)
+{
+	int i;
+		if ((archivo = fopen("archivo.txt", "r")) == NULL)
+		{
+			producto = creaProductos (producto);
+			llenaArchivo (archivo, producto);	
+		}
+		else
+		{
+			producto = (producto_t *) calloc(20, sizeof(producto_t));
+			for (i = 0; i < 20; i ++)
+			{
+				fscanf(archivo, " %[^\n]", producto[i].nombre);
+				fscanf(archivo, "%f\t", &producto[i].precio);
+				fscanf(archivo, "%d\n", &producto[i].cantidad);
+			}
+			fclose(archivo);
+		}
+	return producto;
+}
+
 
 producto_t *creaProductos (producto_t *producto)
 {
@@ -105,7 +133,7 @@ producto_t *creaProductos (producto_t *producto)
 	strcpy(producto[1].nombre, "manzana");
 	producto[1].precio = 40;
 	strcpy(producto[2].nombre, "aguacate");
-	producto[2].precio = 9999.00;
+	producto[2].precio = 9999.99;
 	strcpy(producto[3].nombre, "maruchan");
 	producto[3].precio = 7.50;
 	strcpy(producto[4].nombre, "condones");
@@ -141,25 +169,24 @@ producto_t *creaProductos (producto_t *producto)
 	strcpy(producto[19].nombre, "sal");
 	producto[19].precio = 10.00;
 	for (i = 0; i < 20; i++)
-		producto[i].cantidad = 50;
+		producto[i].cantidad = 1000;
 	return producto;
 }
 
 void llenaArchivo (FILE *archivo, producto_t *producto)
 {
 	int i;
-	if ((archivo = fopen("archivo.txt", "r")) == NULL)
-		if ((archivo = fopen("archivo.txt", "a+")) == NULL)
-		{
-			printf("\n\n\t\tERROR: No se pudo manipular el archivo.\nEl programa se cerrará\n");
-			exit(0);
-		}
-		else
-		{
-			fprintf(archivo, "\tPRODUCTO    PRECIO\tCANTIDAD\n\n");
-			for (i = 0; i < 20; i++)
-				fprintf(archivo, "%16s%10.2f\t%d\n", producto[i].nombre, producto[i].precio, producto[i].cantidad);
-		}
+
+	if ((archivo = fopen("archivo.txt", "w+")) == NULL)
+	{
+		printf("\n\n\t\tERROR: No se pudo manipular el archivo.\nEl programa se cerrará\n");
+		exit(0);
+	}
+	else
+	{
+		for (i = 0; i < 20; i++)
+			fprintf(archivo, "%s\n%f\n%d\n", producto[i].nombre, producto[i].precio, producto[i].cantidad);
+	}
 
 	fclose(archivo);
 	return;
@@ -168,20 +195,74 @@ void llenaArchivo (FILE *archivo, producto_t *producto)
 
 void imprimeInventario (FILE *archivo, producto_t *producto)
 {
-	if ((archivo = fopen("archivo.txt", "a+")) == NULL)
+	int i;
+
+	if ((archivo = fopen("archivo.txt", "r")) == NULL)
 	{
 		printf("\n\n\n\tERROR: No se pudo manipular el archivo.\nEl programa se cerrará\n");
 		exit(0);
 	}
 	else 
 	{
-		char c = fgetc(archivo);
-		while (c != -1)
+		for (i = 0; i < 20; i ++)
 		{
-			printf("%c", c);
-			c = fgetc(archivo);
+			printf("%16s%10.2f%10d\n", producto[i].nombre, producto[i].precio, producto[i].cantidad);
 		}
 	}
 	fclose(archivo);
 	return;
+}
+
+producto_t * actualizaCantidad (FILE *archivo, producto_t *producto)
+{
+	char actualiza[15];
+	int i, nuevaCantidad;
+
+	system("clear");
+	printf("\n\nINVENTARIO ACTUAL DE 'EL PREBE INFELIZ':\n\n");
+	printf("\tPRODUCTO    PRECIO\tCANTIDAD\n\n");
+	imprimeInventario (archivo, producto);
+	printf("\n\t\t¿De qué producto deseas actualizar la cantidad?\n\t\t\t -> ");
+	scanf(" %[^\n]", actualiza);
+
+	for (i = 0; i < 20; i ++)
+		if (strcmp(actualiza, producto[i].nombre) == 0)
+		{
+			printf("\n\t\t¿Con cuántos productos de '%s' se cuenta ahora? ", actualiza);
+			scanf("%d", &nuevaCantidad);
+			producto[i].cantidad = nuevaCantidad;
+			return producto;
+		}
+	printf("\n\t\tEl producto ingresado no existe.\n\tPresiona una tecla para volver al menú principal.");
+	getchar();
+	getchar();
+	return producto;
+	
+}
+
+producto_t * actualizaPrecio (FILE *archivo, producto_t *producto)
+{
+	char actualiza[15];
+	int i;
+	float nuevoPrecio;
+
+	system("clear");
+	printf("\n\nINVENTARIO ACTUAL DE 'EL PREBE INFELIZ':\n\n");
+	printf("\tPRODUCTO    PRECIO\tCANTIDAD\n\n");
+	imprimeInventario (archivo, producto);
+	printf("\n\t\t¿De qué producto deseas actualizar la cantidad?\n\t\t\t -> ");
+	scanf(" %[^\n]", actualiza);
+
+	for (i = 0; i < 20; i ++)
+		if (strcmp(actualiza, producto[i].nombre) == 0)
+		{
+			printf("\n\t\t¿Cuánto cuesta '%s' ahora? ", actualiza);
+			scanf("%f", &nuevoPrecio);
+			producto[i].precio = nuevoPrecio;
+			return producto;
+		}
+	printf("\n\t\tEl producto ingresado no existe.\n\tPresiona una tecla para volver al menú principal.");
+	getchar();
+	getchar();
+	return producto;
 }
